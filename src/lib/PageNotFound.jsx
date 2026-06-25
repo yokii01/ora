5632ae6f08ea6aa14495eb3f154dc16d4f4d462b
nexdecadeinnovation <scorpix202@gmail.com>
2026-06-25 09:59:50 +0530

    Fix React import in PageNotFound causing app crash on missing routes

--- src/lib/PageNotFound.jsx
+++ src/lib/PageNotFound.jsx
@@ -1,7 +1,7 @@
 const db = globalThis.__B44_DB__ || { auth:{ isAuthenticated: async()=>false, me: async()=>null }, entities:new Proxy({}, { get:()=>({ filter:async()=>[], get:async()=>null, create:async()=>({}), update:async()=>({}), delete:async()=>({}) }) }), integrations:{ Core:{ UploadFile:async()=>({ file_url:'' }) } } };
 
+import React from 'react';
 import { useLocation } from 'react-router-dom';
-
 import { useQuery } from '@tanstack/react-query';
 
 export default function PageNotFound({}) {
